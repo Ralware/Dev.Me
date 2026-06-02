61665abc1c45951fb2bab53ed26d9d07e8af21f4
Ral <ral.vulns@gmail.com>
2026-06-02 17:08:38 +0530

    Rock Paper Scissors - COMPACT

--- JavaScript Practice/Rock Paper Scissors - Console Edition/Rock-Paper-Scissors-Engine.js
+++ JavaScript Practice/Rock Paper Scissors - Console Edition/Rock-Paper-Scissors-Engine.js
@@ -1,8 +1,10 @@
 let RandomOutput;
 let Wins = 0;
 let Losses = 0;
+let Draws = 0;
 
-function ComputerMove() {
+function GameEngine(UserInput) {
+
     let RandomChoice = Math.random();
     if (RandomChoice < 1 / 3) {
         RandomOutput = "Rock";
@@ -11,53 +13,35 @@ function ComputerMove() {
     } else {
         RandomOutput = "Scissors";
     }
-}
 
-function RockOutput() {
-    if (RandomOutput === "Rock") {
-        console.log("It's A DRAW!\nComputer Chose: " + RandomOutput + "\nYou Chose: Rock",);
-        document.getElementById("Result").textContent = "It's A DRAW! , Computer Chose: " + RandomOutput + " , You Chose: Rock";
-        document.getElementById("Count").textContent = "Wins : " + Wins + " Losses : " + Losses;
-    } else if (RandomOutput === "Paper") {
-        console.log("You Have LOST!\nComputer Chose: " + RandomOutput + "\nYou Chose: Rock",);
+    if (UserInput === RandomOutput) {
+        DrawPrint(UserInput);
+    } else if ((UserInput === "Rock" && RandomOutput === "Paper") || (UserInput === "Paper" && RandomOutput === "Scissors") || (UserInput === "Scissors" && RandomOutput === "Rock")) {
+        LossPrint(UserInput);
+    } else if ((UserInput === "Rock" && RandomOutput === "Scissors") || (UserInput === "Paper" && RandomOutput === "Rock") || (UserInput === "Scissors" && RandomOutput === "Paper")) {
+        WinPrint(UserInput);
 
-        document.getElementById("Result").textContent = "You Have LOST! , Computer Chose: " + RandomOutput + " , You Chose: Rock";
-        document.getElementById("Count").textContent = "Wins : " + Wins + " Losses : " + (Losses += 1);
-    } else {
-        console.log("You Have WON!\nComputer Chose: " + RandomOutput + "\nYou Chose: Rock",);
-        document.getElementById("Result").textContent = "You Have WON! , Computer Chose: " + RandomOutput + " , You Chose: Rock";
-        document.getElementById("Count").textContent = "Wins : " + (Wins += 1) + " Losses : " + Losses;
     }
 }
 
-function PaperOutput() {
-    if (RandomOutput === "Rock") {
-        console.log("You Have WON!\nComputer Chose: " + RandomOutput + "\nYou Chose: Paper",);
-        document.getElementById("Result").textContent = "You Have WON! , Computer Chose: " + RandomOutput + " , You Chose: Paper";
-        document.getElementById("Count").textContent = "Wins : " + (Wins += 1) + " Losses : " + Losses;
-    } else if (RandomOutput === "Paper") {
-        console.log("It's A DRAW!\nComputer Chose: " + RandomOutput + "\nYou Chose: Paper",);
-        document.getElementById("Result").textContent = "It's A DRAW! , Computer Chose: " + RandomOutput + " , You Chose: Paper";
-        document.getElementById("Count").textContent = "Wins : " + Wins + " Losses : " + Losses;
-    } else {
-        console.log("You Have LOST!\nComputer Chose: " + RandomOutput + "\nYou Chose: Paper",);
-        document.getElementById("Result").textContent = "You Have LOST! , Computer Chose: " + RandomOutput + " , You Chose: Paper";
-        document.getElementById("Count").textContent = "Wins : " + Wins + " Losses : " + (Losses += 1);
-    }
+function DrawPrint(UserInput) {
+    console.log("It's A DRAW!\nComputer Chose: " + RandomOutput + "\nYou Chose: " + UserInput);
+    document.getElementById("Result").textContent = "It's A DRAW! , Computer Chose: " + RandomOutput + " , You Chose: " + UserInput;
+    document.getElementById("Count").textContent = "Wins : " + Wins + " Losses : " + Losses + " Draws : " + (Draws += 1);
 }
 
-function ScissorsOutput() {
-    if (RandomOutput === "Rock") {
-        console.log("You Have LOST!\nComputer Chose: " + RandomOutput + "\nYou Chose: Scissors",);
-        document.getElementById("Result").textContent = "You Have LOST! , Computer Chose: " + RandomOutput + " , You Chose: Scissors";
-        document.getElementById("Count").textContent = "Wins : " + Wins + " Losses : " + (Losses += 1);
-    } else if (RandomOutput === "Paper") {
-        console.log("You Have WON!\nComputer Chose: " + RandomOutput + "\nYou Chose: Scissors",);
-        document.getElementById("Result").textContent = "You Have WON! , Computer Chose: " + RandomOutput + " , You Chose: Scissors";
-        document.getElementById("Count").textContent = "Wins : " + (Wins += 1) + " Losses : " + Losses;
-    } else {
-        console.log("It's A DRAW!\nComputer Chose: " + RandomOutput + "\nYou Chose: Scissors",);
-        document.getElementById("Result").textContent = "It's A DRAW! , Computer Chose: " + RandomOutput + " , You Chose: Scissors";
-        document.getElementById("Count").textContent = "Wins : " + Wins + " Losses : " + Losses;
-    }
+function WinPrint(UserInput) {
+    console.log("You Have WON!\nComputer Chose: " + RandomOutput + "\nYou Chose: " + UserInput);
+    document.getElementById("Result").textContent = "You Have WON! , Computer Chose: " + RandomOutput + " , You Chose: " + UserInput;
+    document.getElementById("Count").textContent = "Wins : " + (Wins += 1) + " Losses : " + (Losses) + " Draws : " + (Draws);
 }
+
+function LossPrint(UserInput) {
+    console.log("You Have LOST!\nComputer Chose: " + RandomOutput + "\nYou Chose: " + UserInput);
+    document.getElementById("Result").textContent = "You Have LOST! , Computer Chose: " + RandomOutput + " , You Chose: " + UserInput;
+    document.getElementById("Count").textContent = "Wins : " + Wins + " Losses : " + (Losses += 1) + " Draws : " + (Draws);
+}
+
+
+
+
