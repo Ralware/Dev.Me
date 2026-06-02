cae6222d36ee9e2c78afe4ef1410b0b0d17dbed1
Ral <ral.vulns@gmail.com>
2026-06-02 18:24:12 +0530

    Finished The Outcome Message Display

--- JavaScript Practice/Rock Paper Scissors - Console Edition/Rock-Paper-Scissors-Engine.js
+++ JavaScript Practice/Rock Paper Scissors - Console Edition/Rock-Paper-Scissors-Engine.js
@@ -27,16 +27,31 @@ function GameEngine(UserInput) {
 function DrawPrint(UserInput) {
     document.getElementById("Result").textContent = "Draw";
     document.getElementById("Draws").textContent = "Draws : " + (Draws += 1);
+    document.getElementById("Outcome").textContent = "You Chose : " + UserInput + " | Computer Chose : "+ RandomOutput;
 }
 
 function WinPrint(UserInput) {
     document.getElementById("Result").textContent = "You Win";
     document.getElementById("Wins").textContent = "Wins : " + (Wins += 1);
+    document.getElementById("Outcome").textContent = "You Chose : " + UserInput + " | Computer Chose : "+ RandomOutput;
 }
 
 function LossPrint(UserInput) {
     document.getElementById("Result").textContent = "You Lose";
     document.getElementById("Losses").textContent = "Losses : " + (Losses += 1);
+    document.getElementById("Outcome").textContent = "You Chose : " + UserInput + " | Computer Chose : "+ RandomOutput;
+}
+
+function ResetFunction() {
+    Wins = 0;
+    Losses = 0;
+    Draws = 0;
+
+    document.getElementById("Wins").textContent = "Wins : 0";
+    document.getElementById("Losses").textContent = "Losses : 0";
+    document.getElementById("Draws").textContent = "Draws : 0";
+    document.getElementById("Outcome").textContent = "Outcome Shows Up Here";
+    document.getElementById("Result").textContent = "Start Playing !";
 }
 
 
