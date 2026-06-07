2e7ab66b8b48c8fb4a8500019de819e8edb972ea
Ral <ral.vulns@gmail.com>
2026-06-07 19:30:24 +0530

    New Done Feature

--- Project - To Do/To-Do.js
+++ Project - To Do/To-Do.js
@@ -9,7 +9,7 @@ function AddTask() {
         return;
     }
     Tasks.push({
-        TaskName: TaskValue, DueDate: DueDateValue
+        TaskName: TaskValue, DueDate: DueDateValue , done:false
     });
     localStorage.setItem("Tasks", JSON.stringify(Tasks));
     document.getElementById('Input').value = '';
@@ -17,10 +17,11 @@ function AddTask() {
     DisplayTasks();
 
 }
-function UpdateLocalStorage()
-{
-    localStorage.setItem("Tasks",JSON.stringify(Tasks));
+
+function UpdateLocalStorage() {
+    localStorage.setItem("Tasks", JSON.stringify(Tasks));
 }
+
 function DisplayTasks() {
 
     let InnerHTMLDiv = '';
@@ -29,21 +30,32 @@ function DisplayTasks() {
         const TaskToPrint = Tasks[i]; // Object
         const TaskName = TaskToPrint.TaskName;
         const DueDate = TaskToPrint.DueDate;
+        const isDone = TaskToPrint.done;
+
         const InnerHTML = `<div class="Task-Row">
-                                      <div class="Task-Input-Name">
-                                        ${TaskName}
-                                      </div>
-                                      <div class="Date-Delete">
-                                         <div>${DueDate}</div>
-                                      <div>
-                                      <button onclick="Tasks.splice(${i},1); UpdateLocalStorage(); DisplayTasks();" class="Delete-Button">Delete</button>
-                                      </div>
-                                      </div>
-                                   </div>`;
+                    <div class="Task-Input-Name ${isDone ? 'Done-Decoration' : ''}" id="Task-Text-${i}">
+                        ${TaskName}
+                    </div>
+                    <div class="Date-Delete">
+                        <div>${DueDate}</div>
+                        <div>
+                            <button onclick="Tasks.splice(${i},1); UpdateLocalStorage(); DisplayTasks();" class="Delete-Button">Delete</button>
+                        </div>
+                        <div>
+                            <button class="Done-Button ${isDone ? 'Done-Color' : ''}" id="Done-${i}" onclick="ToggleDone(${i})">Done</button>
+                        </div>
+                    </div>
+                </div>`;
         InnerHTMLDiv += InnerHTML;
     }
 
     document.getElementById('Tasks-Div').innerHTML = InnerHTMLDiv;
 }
+function ToggleDone(i) {
+    Tasks[i].done = !Tasks[i].done; // Changing False -> True On Done Button Click
+    UpdateLocalStorage();
+    DisplayTasks();
+}
 
 DisplayTasks();
+
