a19714ec639b73e7d29e90be5c89b55b2821e5b3
Ral <ral.vulns@gmail.com>
2026-06-07 15:08:14 +0530

    Small Bug Fix - Task Title Could Still Accept Multiple Space As An INPUT

--- Project - To Do/To-Do.js
+++ Project - To Do/To-Do.js
@@ -1,23 +1,26 @@
 document.getElementById('Add').addEventListener("click", AddTask);
 
-const Tasks = [];
+const Tasks = JSON.parse(localStorage.getItem("Tasks")) || [];
 
 function AddTask() {
     const TaskValue = document.getElementById('Input').value;
     const DueDateValue = document.getElementById('DueDate').value;
-    if (TaskValue === '') {
+    if (!TaskValue.trim()) {
         return;
     }
     Tasks.push({
         TaskName: TaskValue, DueDate: DueDateValue
     });
-
+    localStorage.setItem("Tasks", JSON.stringify(Tasks));
     document.getElementById('Input').value = '';
     document.getElementById('DueDate').value = '';
     DisplayTasks();
 
 }
-
+function UpdateLocalStorage()
+{
+    localStorage.setItem("Tasks",JSON.stringify(Tasks));
+}
 function DisplayTasks() {
 
     let InnerHTMLDiv = '';
@@ -33,7 +36,7 @@ function DisplayTasks() {
                                       <div class="Date-Delete">
                                          <div>${DueDate}</div>
                                       <div>
-                                      <button onclick="Tasks.splice(${i},1); DisplayTasks();" class="Delete-Button">Delete</button>
+                                      <button onclick="Tasks.splice(${i},1); UpdateLocalStorage(); DisplayTasks();" class="Delete-Button">Delete</button>
                                       </div>
                                       </div>
                                    </div>`;
@@ -42,3 +45,5 @@ function DisplayTasks() {
 
     document.getElementById('Tasks-Div').innerHTML = InnerHTMLDiv;
 }
+
+DisplayTasks();
