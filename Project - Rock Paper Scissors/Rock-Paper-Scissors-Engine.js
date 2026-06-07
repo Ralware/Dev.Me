d8aaafbb5b7895d91368a8df4ebc92af2afd9ce7
Ral <ral.vulns@gmail.com>
2026-06-07 20:41:38 +0530

    Formatted

--- Project - Rock Paper Scissors/Rock-Paper-Scissors-Engine.js
+++ Project - Rock Paper Scissors/Rock-Paper-Scissors-Engine.js
@@ -68,30 +68,30 @@ function ResetPrint() {
     document.querySelector('.Output').innerHTML = '';
 }
 function PrintChoice(UserInput) {
-    if (UserInput === 'Rock'){
+    if (UserInput === 'Rock') {
         document.getElementById("You-Choose-Text").textContent = "Your Choice : ";
         document.querySelector('.Choice').innerHTML = '<img alt="Your Choice" class="Icons" src="Sources/Rock.Icon.png">';
     }
-    else if (UserInput === 'Paper'){
+    else if (UserInput === 'Paper') {
         document.getElementById("You-Choose-Text").textContent = "Your Choice : ";
         document.querySelector('.Choice').innerHTML = '<img alt="Your Choice" class="Icons" src="Sources/Paper.Icon.png">';
     }
-    else if (UserInput === 'Scissors'){
+    else if (UserInput === 'Scissors') {
         document.getElementById("You-Choose-Text").textContent = "Your Choice : ";
         document.querySelector('.Choice').innerHTML = '<img alt="Your Choice" class="Icons" src="Sources/Scissors.Icon.png">';
     }
 }
 
 function PrintComputerChoice() {
-    if (RandomOutput === 'Rock'){
+    if (RandomOutput === 'Rock') {
         document.getElementById("Computer-Chose-Text").textContent = "Computer Choice : ";
         document.querySelector('.Output').innerHTML = '<img alt="Your Choice" class="Icons" src="Sources/Rock.Icon.png">';
     }
-    else if (RandomOutput === 'Paper'){
+    else if (RandomOutput === 'Paper') {
         document.getElementById("Computer-Chose-Text").textContent = "Computer Choice : ";
         document.querySelector('.Output').innerHTML = '<img alt="Your Choice" class="Icons" src="Sources/Paper.Icon.png">';
     }
-    else if (RandomOutput === 'Scissors'){
+    else if (RandomOutput === 'Scissors') {
         document.getElementById("Computer-Chose-Text").textContent = "Computer Choice : ";
         document.querySelector('.Output').innerHTML = '<img alt="Your Choice" class="Icons" src="Sources/Scissors.Icon.png">';
     }
